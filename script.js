fb5fe5cb0092d448180131bcbba027845f8418ee
Alvin Toch <Alvinstoch@live.com>
2022-10-15 12:56:51 -0700

    about a 30% into the game logic?

--- script.js
+++ script.js
@@ -17,9 +17,28 @@ for (let i=0; i<input.length; i++){
 }
 
 for(let j = 0; j<boardSquare.length; j++){
-    boardSquare[j].addEventListener('click',()=>{
+    for(let i = 1; i<4; i++){
+        //for(let k=0; k<9;k++){
+    boardSquare[j].addEventListener('click',(e)=>{
+        // if (e.target.classList[1].innerHTML === holding.id){
+        //     console.log(e.target)
+        //     return
+        // } else {
+       
         boardSquare[j].innerHTML=holding.id
-        return;
+        console.log(boardSquare[j].classList.contains(`${e.target.classList[1]}`))
+        console.log("hi")
+       
+        
+        return
     }
     )
-}
+}}
+//save for later reference
+// identifying the class list
+//boardSquare[j].classList[1]+ boardSquare[j].classList[2]+ boardSquare[j].classList[3]
+// boardSquare[j].classList[i] && boardSquare[j].innerText
+        //checking the clicked square's stats
+//e.target.classList[i] 
+
+//e.target.innerHTML
