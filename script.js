e1f0df8e77c25c999fea499b59df90204dbaedbd
Alvin Toch <Alvinstoch@live.com>
2022-10-16 09:54:37 -0700

    beta completion check, inhibitor still not reinitializing

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ for (let i=0; i<input.length; i++){
     input[i].addEventListener('click',()=>{
        
        console.log("hello " + input[i].innerText)
-        
+        playingValue = input[i].innerText
         holding.setAttribute('id', input[i].value) 
         console.log(holding.id)
         return
@@ -20,34 +20,37 @@ const completionCheck =()=>{
     for (let i=0; i <boardSquare.length; i++){
     if( boardSquare[i].innerText !== ""){
         completed = true
+        console.log(completed)
         return
     }
 }
 }
+
+
+
 //the inhibitor is only checking the original input before just bypassing the check
 
 for(let i = 0; i<boardSquare.length; i++){    
-        boardSquare[i].addEventListener('click',function(e){  
-    for(let j = 1; j<4; j++){    
-        for(let k=0; k<9;k++){    //this doesn't need to be repeated three times but because it won't loop it's just here
-     if         (   document.querySelectorAll(`.${e.target.classList[1]}`)[k].innerText === holding.id ){
-        console.log("check 1")
-        return
-     } else if  (   document.querySelectorAll(`.${e.target.classList[2]}`)[k].innerText === holding.id ){
-        console.log("check 2")
-     } else if  (   document.querySelectorAll(`.${e.target.classList[3]}`)[k].innerText === holding.id ){
-        console.log("check 3")
-        return
-        // pretty sure the check isn't being reinitialized everytime
-    }else  {
-        
-        boardSquare[i].innerHTML=holding.id
-        console.log("hi")
+    boardSquare[i].addEventListener('click',function(e){  
 
-         }
-        }
+for(let j = 1; j<4; j++){ 
+    for(let k=0; k<9;k++){   
+ if (   document.querySelectorAll(`.${e.target.classList[1]}`)[k].innerHTML === holding.id ||
+        document.querySelectorAll(`.${e.target.classList[2]}`)[k].innerHTML === holding.id ||
+        document.querySelectorAll(`.${e.target.classList[3]}`)[k].innerHTML === holding.id ){
+        console.log( document.querySelectorAll(`.${e.target.classList[1]}`))
+        console.log( document.querySelectorAll(`.${e.target.classList[2]}`))
+        console.log( document.querySelectorAll(`.${e.target.classList[3]}`))
+        return
+}else {
+    
+    boardSquare[i].innerHTML=holding.id
+    console.log("bypassed")
+    return
      }
-    })
+    }
+ }
+})
 }
 //save for later reference
 // identifying the class list
