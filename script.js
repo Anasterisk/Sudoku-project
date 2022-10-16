8d090cc69630aaf314b9907fd639ed534e520bd7
Alvin Toch <Alvinstoch@live.com>
2022-10-15 17:21:54 -0700

    beta completion check, troubleshooting inhibitor

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let input= document.querySelectorAll(".input")
 let playingValue = ''
 let holding = document.querySelector(".holding")
 let boardSquare = document.querySelectorAll('.bl')
-
+let completed = false
 
 for (let i=0; i<input.length; i++){
     input[i].addEventListener('click',()=>{
@@ -15,23 +15,35 @@ for (let i=0; i<input.length; i++){
     })
     
 }
+
+const completionCheck =()=>{
+    for (let i=0; i <boardSquare.length; i++){
+    if( boardSquare[i].innerText !== ""){
+        completed = true
+        return
+    }
+}
+}
 //the inhibitor is only checking the original input before just bypassing the check
+
 for(let i = 0; i<boardSquare.length; i++){    
         boardSquare[i].addEventListener('click',function(e){  
     for(let j = 1; j<4; j++){    
         for(let k=0; k<9;k++){    //this doesn't need to be repeated three times but because it won't loop it's just here
-     if (   document.querySelectorAll(`.${e.target.classList[1]}`)[k].innerHTML === holding.id ||
-            document.querySelectorAll(`.${e.target.classList[2]}`)[k].innerHTML === holding.id ||
-            document.querySelectorAll(`.${e.target.classList[3]}`)[k].innerHTML === holding.id ){
-            console.log(e.target.classList)
-            return
-            // pretty sure the check isn't being reinitialized everytime
-    }else if(document.querySelectorAll(`.${e.target.classList[j]}`)[k].innerHTML !== holding.id ) 
-    {
+     if         (   document.querySelectorAll(`.${e.target.classList[1]}`)[k].innerText === holding.id ){
+        console.log("check 1")
+        return
+     } else if  (   document.querySelectorAll(`.${e.target.classList[2]}`)[k].innerText === holding.id ){
+        console.log("check 2")
+     } else if  (   document.querySelectorAll(`.${e.target.classList[3]}`)[k].innerText === holding.id ){
+        console.log("check 3")
+        return
+        // pretty sure the check isn't being reinitialized everytime
+    }else  {
         
         boardSquare[i].innerHTML=holding.id
         console.log("hi")
-        return
+
          }
         }
      }
@@ -52,3 +64,4 @@ for(let i = 0; i<boardSquare.length; i++){
 //
 //document.querySelectorAll(`.${e.target.classList[i]}`)[k].innerHTML
 //^^ this code checks for all 27 spaces (region, row, and column) for it's innerHTML.
+
