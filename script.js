764aa1a72e671d8667b9397b3ca67629af206c37
Alvin Toch <Alvinstoch@live.com>
2022-10-17 12:45:44 -0700

    winning conditions 20%, peripherals 20%

--- script.js
+++ script.js
@@ -9,7 +9,21 @@ let int =null
 let input= document.querySelectorAll(".input")
 let holding = document.querySelector(".holding")
 let boardSquare = document.querySelectorAll('.bl')
+//win switch
 let completed   = false
+let mockBoard   = [
+    "","","","","","","","","",
+    "","","","","","","","","",
+    "","","","","","","","","",
+    "","","","","","","","","",
+    "","","","","","","","","",
+    "","","","","","","","","",
+    "","","","","","","","","",
+    "","","","","","","","","",
+    "","","","","","","","","",
+]
+let matrix   = []
+//inhibitor check variables
 let legalMove   = false
 let checkSq     = null
 let checkRow    = null
@@ -49,7 +63,6 @@ const clocking=()=>{
 // selecting input value//
 for (let i=0; i<input.length; i++){
     input[i].addEventListener('click',()=>{
-       
        console.log(`checking input button ${input[i].innerText}`)
         playingValue = input[i].innerText
         holding.setAttribute('id', input[i].value) 
@@ -59,18 +72,38 @@ for (let i=0; i<input.length; i++){
     })
     
 }
+// const matrixCal=(mockBoard,x)=>{
+//     let matrix =[],i,k;
+//      x=9
+//     for(i = 0, k=-1;i<mockBoard.length;i++){
+//         if(i%x ===0){
+//             k++;
+//             matrix[k]=[];
+//         }
+//         matrix[k].push(list[i]);
+//     }
+//     return matrix
+// }
 
 // game stopping function // 
-const completionCheck =()=>{
-    for (let i=0; i <boardSquare.length; i++){
-    if( boardSquare[i].innerText !== ""){
-        completed = true
-        console.log(completed)
+const completionCheck =()=>{ 
+  
+    if( completed === false){
+         // console.log("Com Check 1 ") 
+    //for (let i=0; i <8; i++){
+    //if( matrix[i].reduce((total,x)=>{
+       //return total=+x
+    //},0)===45){
+        //completed = true
+        console.log("com check 1.5")
         stopTimer
-        return
+    }else{
+        
+        console.log(`Com Check 2 ${completed}`)
+        
     }
 }
-}
+
   // testing function calls//
 const testFunct=()=>{
     console.log("test function is running")
@@ -85,19 +118,19 @@ const inhibitorCheck=()=>{
         console.log("inhibitor check 2")
     //for(let j = 1; j<4; j++){ 
         for(let k=0; k<9;k++){
-           console.log("inhibitor check 3 attempt")
+           console.log(`inhibitor check 3 attempt: ${[k]}`)
     if( checkSq[k].innerHTML === holding.id ||
         checkRow[k].innerHTML === holding.id||     
         checkCol[k].innerHTML === holding.id){
         
         
         
-        console.log("inhibitor check 3 blocked")
+        console.log(`inhibitor check 3 blocked try:${[k]}`)
         legalMove = false 
         return     
         }else{
         
-        console.log("inhibitor check 3 passed")
+        console.log(`inhibitor check 3 passed try: ${[k]}`)
         legalMove = true
         }
     }}
@@ -106,7 +139,7 @@ const inhibitorCheck=()=>{
 
 //Game logic//
                 //the functions aren't being called
-for(let i = 0; i<boardSquare.length; i++){    
+for(let i = 0; i<boardSquare.length; i++){ 
     boardSquare[i].addEventListener('click',function(e){       
     if ( legalMove === false){
                     console.log(`logic check 1, legalMove: ${legalMove}`)
@@ -117,12 +150,15 @@ for(let i = 0; i<boardSquare.length; i++){
                     console.log(`logic check 2, "legalMove: ${legalMove}"`)
         if (legalMove === true){       
         boardSquare[i].innerHTML=holding.id
+        mockBoard[i]= holding.id
+                    console.log(mockBoard[i])
                     console.log(`logic check 3 "bypassed", legalMove: ${legalMove}`)
         legalMove   = false
         checkSq     = null
         checkRow    = null
         checkCol    = null
                     console.log(legalMove)
+        completionCheck()
         return
         }else if(legalMove === false) {
                     console.log (`logic check 3 "invalid move"`)
@@ -135,7 +171,7 @@ for(let i = 0; i<boardSquare.length; i++){
     }
 })}
 
-console.log(document.querySelectorAll(`.${e.target.classList[1]}`)[k].innerHTML);
+
 //save for later reference
 // identifying the class list
 //boardSquare[j].classList[1]+ boardSquare[j].classList[2]+ boardSquare[j].classList[3]
