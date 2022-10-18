af06bf2fd69f30de4ea886f73965b77165f1736c
Alvin Toch <Alvinstoch@live.com>
2022-10-18 09:34:46 -0700

    gamelogic running 100%, peripherals in the work

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 
 //let playingValue = ''
 //timer variables
-let [seconds,minutes,hours] = [0,0,0]
+// let seconds = 0
+// let minutes = 0
+// let hours   =0
+let [milliseconds,seconds,minutes,hours] = [0,0,0,0];
 let timer = document.querySelector('.timer')
 let int =null
 
@@ -9,20 +12,23 @@ let int =null
 let input= document.querySelectorAll(".input")
 let holding = document.querySelector(".holding")
 let boardSquare = document.querySelectorAll('.bl')
+let identifier = null
 //win switch
 let completed   = false
-let mockBoard   = [
-    "","","","","","","","","",
-    "","","","","","","","","",
-    "","","","","","","","","",
-    "","","","","","","","","",
-    "","","","","","","","","",
-    "","","","","","","","","",
-    "","","","","","","","","",
-    "","","","","","","","","",
-    "","","","","","","","","",
-]
-let matrix   = []
+let boardBalance   = 0
+
+//shortcut fill//
+let skip = document.querySelector(".skip")
+let quickFill =[
+    1,2,3,9,7,8,5,6,4,
+    4,5,6,3,1,2,8,9,7,
+    7,8,9,6,4,5,2,3,1,
+    3,1,2,8,9,7,4,5,6,
+    6,4,5,2,3,1,7,8,9,
+    9,7,8,5,6,4,1,2,3,
+    2,3,1,7,8,9,6,4,5,
+    5,6,4,1,2,3,9,7,8,
+    8,9,7,4,5,6,3,1,2,]
 //inhibitor check variables
 let legalMove   = false
 let checkSq     = null
@@ -32,42 +38,53 @@ let checkCol    = null
 // game timer// 
 //https://www.foolishdeveloper.com/2021/10/simple-stopwatch-using-javascript.html used it a reference still not working though
     // starting function//
- const startTimer=()=>{
-    if (int!== null){
+ const startTimer=()=> {
+    console.log("timer Starts")
+    if (completed===true){
+        return
+    }
+    else if (int!== null){
         clearInterval(int)
     }
-    int = setInterval(timer,10)
-    console.log("timer Starts")
+    int = setInterval(timer,1000)
  }
     // stoping function//
 const stopTimer =()=>{
     clearInterval(int)
 }
+    // resetting time//
+const resetTimer=()=>{
+    clearInterval(int)
+    [seconds,minutes,hours]= [0,0,0];
+    timer.innerHTML ="00:00:00"
+}
     // timer innards//
 const clocking=()=>{
-    seconds ++
-    if( seconds === 60){
-        seconds = 0
-        minutes++
-        if(minutes === 60){
-            minutes = 0
-            hours ++
+    milliseconds+=10;
+    if(milliseconds===1000){
+        milliseconds = 0;
+        seconds ++
+        if( seconds === 60){
+            seconds = 0
+            minutes++
+            if(minutes === 60){
+                minutes = 0
+                hours ++
         }
     }
+}
     let h = hours <10   ?"0"+hours: hours
-    let m = minute <10  ?"0"+minutes: minutes
+    let m = minutes <10  ?"0"+minutes: minutes
     let s = seconds <10 ?"0"+seconds: seconds
-    timer.innerHTML=`${h}:${m}:${s}`
+    return timer.innerHTML=`${h}:${m}:${s}`
 }
 
 // selecting input value//
 for (let i=0; i<input.length; i++){
     input[i].addEventListener('click',()=>{
-       console.log(`checking input button ${input[i].innerText}`)
         playingValue = input[i].innerText
         holding.setAttribute('id', input[i].value) 
-        console.log(`holding.id value check ${holding.id}`)
-        startTimer
+        startTimer()
         return
     })
     
@@ -85,22 +102,20 @@ for (let i=0; i<input.length; i++){
 //     return matrix
 // }
 
-// game stopping function // 
+// game stopping functions // 
+const boardCal = (x)=>{
+    y = parseInt(identifier) || 0
+    console.log(y)
+    x = parseInt(holding.id) || 0
+    console.log(x)
+    return boardBalance+=x- y
+}
+
 const completionCheck =()=>{ 
-  
-    if( completed === false){
-         // console.log("Com Check 1 ") 
-    //for (let i=0; i <8; i++){
-    //if( matrix[i].reduce((total,x)=>{
-       //return total=+x
-    //},0)===45){
-        //completed = true
-        console.log("com check 1.5")
-        stopTimer
-    }else{
-        
-        console.log(`Com Check 2 ${completed}`)
-        
+    if(boardBalance===1215){
+        completed = true
+        stopTimer()
+        console.log(`${boardBalance} and ${completed}`)
     }
 }
 
@@ -113,64 +128,70 @@ const testFunct=()=>{
 // inhibitor check functioning//
 const inhibitorCheck=()=>{
     console.log("inhibitor check")
-    //for(let i = 0; i<boardSquare.length; i++){    
-        //boardSquare[i].addEventListener('click',function(e){    
-        console.log("inhibitor check 2")
-    //for(let j = 1; j<4; j++){ 
         for(let k=0; k<9;k++){
-           console.log(`inhibitor check 3 attempt: ${[k]}`)
     if( checkSq[k].innerHTML === holding.id ||
         checkRow[k].innerHTML === holding.id||     
         checkCol[k].innerHTML === holding.id){
-        
-        
-        
-        console.log(`inhibitor check 3 blocked try:${[k]}`)
         legalMove = false 
         return     
         }else{
-        
-        console.log(`inhibitor check 3 passed try: ${[k]}`)
         legalMove = true
         }
-    }}
+    }
+}
 
 
 
 //Game logic//
-                //the functions aren't being called
+
 for(let i = 0; i<boardSquare.length; i++){ 
-    boardSquare[i].addEventListener('click',function(e){       
-    if ( legalMove === false){
+    boardSquare[i].addEventListener('click',function(e){
+        identifier = boardSquare[i].innerHTML
+        completionCheck()
+    if (completed=== true){
+        return
+    }
+    else if ( legalMove === false){
                     console.log(`logic check 1, legalMove: ${legalMove}`)
-                    checkSq = document.querySelectorAll(`.${e.target.classList[1]}`)
-                    checkRow= document.querySelectorAll(`.${e.target.classList[2]}`)
-                    checkCol= document.querySelectorAll(`.${e.target.classList[3]}`)
+        checkSq = document.querySelectorAll(`.${e.target.classList[1]}`)
+        checkRow= document.querySelectorAll(`.${e.target.classList[2]}`)
+        checkCol= document.querySelectorAll(`.${e.target.classList[3]}`)
         inhibitorCheck();
                     console.log(`logic check 2, "legalMove: ${legalMove}"`)
-        if (legalMove === true){       
-        boardSquare[i].innerHTML=holding.id
-        mockBoard[i]= holding.id
-                    console.log(mockBoard[i])
+            if (legalMove === true){ 
+                boardCal();
+                    console.log(`square value before edit: ${boardSquare[i].innerHTML}`)      
+                boardSquare[i].innerHTML=holding.id
                     console.log(`logic check 3 "bypassed", legalMove: ${legalMove}`)
-        legalMove   = false
-        checkSq     = null
-        checkRow    = null
-        checkCol    = null
+                legalMove   = false
+                checkSq     = null
+                checkRow    = null
+                checkCol    = null
+                identifier  = null
                     console.log(legalMove)
-        completionCheck()
-        return
-        }else if(legalMove === false) {
+                    console.log(`total ${boardBalance}`)
+                completionCheck()
+                return
+            }else if(legalMove === false) {
                     console.log (`logic check 3 "invalid move"`)
                     console.log(legalMove)
-        checkSq     = null
-        checkRow    = null
-        checkCol    = null
-        return
+                checkSq     = null
+                checkRow    = null
+                checkCol    = null
+                identifier  = null
+                return
     }
     }
+    
 })}
 
+skip.addEventListener("click",()=> {
+    for(let i=0; i<boardSquare.length; i++)
+    boardSquare[i].innerHTML=quickFill[i]
+    boardBalance = 1215
+    completionCheck()
+}
+)
 
 //save for later reference
 // identifying the class list
